cdd87c38b7c69c1a1098251ab3027c8babf976d8
Jamen <jamen.lang@gmail.com>
2014-06-20 15:05:13 -0600

    Update GarageaTrois-Config.php
    replace " with '

--- GarageaTrois-Config.php
+++ GarageaTrois-Config.php
@@ -2,12 +2,12 @@
 /************ For MYSQL Database (Logging, User authentication and Device authentication)
 Read the README and follow instructions before proceeding ************/
 
-$hostname="localhost"; //replace with database hostname
-$username="USERNAME"; //replace with database username
-$password="PASSWORD"; //replace with database password
-$db_name="garage"; //replace with database name
-$SUPER_SECRET_ADMIN_RESULT="SUPER_SECRET_ADMIN_RESULT"; //replace with whatever is in the res/strings.xml file in the android app.
-$SUPER_SECRET_USER_RESULT="SUPER_SECRET_USER_RESULT"; //replace with whatever is in the res/strings.xml file in the android app.
+$hostname = 'localhost'; //replace with database hostname
+$username = 'USERNAME'; //replace with database username
+$password = 'PASSWORD'; //replace with database password
+$db_name = 'garage'; //replace with database name
+$SUPER_SECRET_ADMIN_RESULT = 'SUPER_SECRET_ADMIN_RESULT'; //replace with whatever is in the res/strings.xml file in the android app.
+$SUPER_SECRET_USER_RESULT = 'SUPER_SECRET_USER_RESULT'; //replace with whatever is in the res/strings.xml file in the android app.
 
 /************ Configuration for Logging ************/
 
@@ -27,7 +27,7 @@ this requires qrlib.php which can be downloaded from http://sourceforge.net/proj
 
 $qr_enabled = '1'; // set to 1 to enable or 0 to disable.
 $qr_size = '250'; // default is 250 pixels
-$apk_link = "files.myawesomedomain.net/garageatrois.apk";
+$apk_link = 'files.myawesomedomain.net/garageatrois.apk';
 
 /************ Configuration for geofence
 Geofencing is a pretty popular form of access restriction based on GPS data and distance between two points. 
@@ -38,13 +38,13 @@ $geofence_return_result = 'true'; //set to true for testing or to attract stalke
 $garage_latitude = '32.9697'; //set to garage latitude
 $garage_longitude = '-96.80322'; //set to garage longitude
 $geofence_unit_of_measurement = 'meters'; // use meters, kilometers or miles;
-$geofence_maximum_allowed_distance= '30'; //distance in units from the garage door latitude/longitude.
+$geofence_maximum_allowed_distance = '30'; //distance in units from the garage door latitude/longitude.
 
 /************ Configuration for notifications ************/
 
-$admin_mobile="4033029392"; //replace with mobile number for recieving text messages
-$admin_email="admin@whereveryouwantthem.com"; //replace with an admin email address
-$notification_email="notifications@fromtheserver.com"; //replace with a notification email address
+$admin_mobile = '4033029392'; //replace with mobile number for recieving text messages
+$admin_email = 'admin@whereveryouwantthem.com'; //replace with an admin email address
+$notification_email = 'notifications@fromtheserver.com'; //replace with a notification email address
 
 
 /************ Location Specific
