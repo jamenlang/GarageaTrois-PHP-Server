6ea0273c5a8006d0a0838835f1003a14437a5eb8
Jamen <jamen.lang@gmail.com>
2015-05-27 10:57:07 -0600

    Update index.php
    show qr if set in config options

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require('GarageaTrois-Config.php');?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,7 +7,25 @@
 </head>
 
 <body>
-<?php echo file_get_contents('GarageaTrois-PHP-Server.php'); ?>
+<?php
+if (!isset($_POST) || empty($_POST)){
+
+        if($qr_enabled == '1'){
+                include('phpqrcode/qrlib.php');
+
+                //might as well generate a qr code for the server address since no post data was received
+                //$link = "my.special.scheme://server=".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+                //OR echo "http://chart.apis.google.com/chart?chf=a,s,000000&chs=200x200&chld=%7C2&cht=qr&chl=" . $link;
+                define('IMAGE_WIDTH',$qr_size);
+                define('IMAGE_HEIGHT',$qr_size);
+                QRcode::png($apk_link);
+                exit;
+        }
+	else{
+		exit;
+	}
+}
+?>
 </body>
 
 </html>
