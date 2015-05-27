06926c07f7ea7cfab56267b04794f09fee55e077
Jamen <jamen.lang@gmail.com>
2015-05-27 10:26:55 -0600

    Create index.php
    just an index file.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>GaT Home</title>
+</head>
+
+<body>
+<?php echo file_get_contents('GarageaTrois-PHP-Server.php'); ?>
+</body>
+
+</html>
