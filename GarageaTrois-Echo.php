0ce4236eb96bf4e01e95ed733cd1ce489287ea33
Jamen <jamen.lang@gmail.com>
2015-09-11 20:47:42 -0600

    Update GarageaTrois-Echo.php

--- GarageaTrois-Echo.php
+++ GarageaTrois-Echo.php
@@ -7,63 +7,66 @@ require('GarageaTrois-Config.php');
  Place in same directory as GarageaTrois-Server and GarageaTrois-Config.php.
 *************************************************************************************/
 
-$dir =  dirname($_SERVER['PHP_SELF']);
+$dir = dirname($_SERVER['PHP_SELF']);
 
 if($_POST){
-        $content = json_encode($_POST);
+	$content = json_encode($_POST);
 
-        $curl = curl_init($hue_url);
-        curl_setopt($curl, CURLOPT_HEADER, false);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HTTPHEADER,
-                array("Content-type: application/json"));
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+	$curl = curl_init($hue_url);
+	curl_setopt($curl, CURLOPT_HEADER, false);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_HTTPHEADER,
+		array("Content-type: application/json")
+	);
+	curl_setopt($curl, CURLOPT_POST, true);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
 
-        $json_response = curl_exec($curl);
+	$json_response = curl_exec($curl);
 
-        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+	$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
-        if ( $status != 201 ) {
-                die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
-        }
+	if ( $status != 201 ) {
+		die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+	}
 
-        curl_close($curl);
-        $response = json_decode($json_response, true);
-        var_dump($response);
-        exit;
+	curl_close($curl);
+	$response = json_decode($json_response, true);
+	var_dump($response);
+	exit;
 }
 
 if(isset($_GET['id']) && $_GET['id'] = 'action'){
-  $switch = $_GET['switch'];
+	$switch = $_GET['switch'];
 
-  $url = "http://$_SERVER[HTTP_HOST]$dir/GarageaTrois-Server.php";
-  $fields = array(
-    'UID' => urlencode($echo_uid),
-    'DID' => urlencode($echo_did),
-    'DeviceName' => urlencode($echo_name),
-    'hasNFC' => urlencode('false'),
-    'switch' => urlencode(ucfirst($_GET['switch']))
-  );
+	$url = "http://$_SERVER[HTTP_HOST]$dir/GarageaTrois-Server.php";
+	$fields = array(
+		'UID' => urlencode($echo_uid),
+		'DID' => urlencode($echo_did),
+		'DeviceName' => urlencode($echo_name),
+		'hasNFC' => urlencode('false'),
+		'switch' => urlencode(ucfirst($_GET['switch'])),
+		'Latitude' => urlencode(ucfirst($garage_latitude)),
+		'Longitude' => urlencode(ucfirst($garage_longitude))
+	);
 
-  //url-ify the data for the POST
-  foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-  rtrim($fields_string, '&');
+	//url-ify the data for the POST
+	foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+	rtrim($fields_string, '&');
 
-  //open connection
-  $ch = curl_init();
+	//open connection
+	$ch = curl_init();
 
-  //set the url, number of POST vars, POST data
-  curl_setopt($ch,CURLOPT_URL, $url);
-  curl_setopt($ch,CURLOPT_POST, count($fields));
-  curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+	//set the url, number of POST vars, POST data
+	curl_setopt($ch,CURLOPT_URL, $url);
+	curl_setopt($ch,CURLOPT_POST, count($fields));
+	curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
 
-  //execute post
-  $result = curl_exec($ch);
+	//execute post
+	$result = curl_exec($ch);
 
-  //close connection
-  curl_close($ch);
-  echo $result;
+	//close connection
+	curl_close($ch);
+	echo $result;
 }
 
 ?>
