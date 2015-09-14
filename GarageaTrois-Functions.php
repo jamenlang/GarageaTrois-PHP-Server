d3f1044cd247d6719f829c1e022fb5affa0aa61a
Jamen <jamen.lang@gmail.com>
2015-09-13 21:23:56 -0600

    Update GarageaTrois-Functions.php

--- GarageaTrois-Functions.php
+++ GarageaTrois-Functions.php
@@ -1,14 +1,9 @@
 <?php
 
 function toggle_relay($gpio_relay){
-        exec('gpio mode 0 out');
-        exec('gpio mode 7 out');
-        exec('gpio mode 8 out');
-        exec('gpio mode 9 out');
-
-        exec("gpio write $gpio_relay 0");
+        exec("/usr/local/bin/gpio write $gpio_relay 0");
         sleep(2);
-        exec("gpio write $gpio_relay 1");
+        exec("/usr/local/bin/gpio write $gpio_relay 1");
 }
 
 function mailer($subject, $message, $newuser){
