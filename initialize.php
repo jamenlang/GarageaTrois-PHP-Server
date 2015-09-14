c72c407272ef3a3edb6e881631f08a5ba5c1de8c
Jamen <jamen.lang@gmail.com>
2015-09-13 20:48:26 -0600

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -7,27 +7,19 @@ if(php_sapi_name() != 'cli'){
 
 include('GarageaTrois/GarageaTrois-Config.php');
 
+if($log_to_file == '1'){
+	//create temp file if it doesn't exist
+	if (!file_exists($log)) {
+		fopen("$log", "w")
+		fwrite($log, 'creating log.');
+		fclose($log);
+	}
+	if (!is_writable($log)) {
+		chmod($log,0777)
+	}
+}
+
 $dir = '/var/www/';
-
-if($use_gpio == true){
-	//gpio will need to be initialized before use.
-	exec("/usr/local/bin/gpio write $other_relay 1");
-	exec("/usr/local/bin/gpio write $light_relay 1");
-	exec("/usr/local/bin/gpio write $door_relay 1");
-	exec("/usr/local/bin/gpio write $lock_relay 1");
-	exec("/usr/local/bin/gpio mode $other_relay OUT");
-	exec("/usr/local/bin/gpio mode $light_relay OUT");
-	exec("/usr/local/bin/gpio mode $door_relay OUT");
-	exec("/usr/local/bin/gpio mode $lock_relay OUT");
-}
-
-while(true){
-	$command="/sbin/ifconfig $configured_interface | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'";
-	$localIP = exec ($command);
-	echo $localIP;
-	if($localIP != '')
-		break;
-}
 
 $files = scandir($dir);
 foreach($files as $filename){
@@ -43,14 +35,47 @@ foreach($files as $filename){
 	}
 }
 
-if(!$gat){
-	exec('git clone https://github.com/jamenlang/GarageaTrois-PHP-Server.git GarageaTrois',$output);
-	file_put_contents($logfile, $output);
+if($use_gpio == true){
+	//gpio will need to be initialized before use.
+	exec("/usr/local/bin/gpio readall", $output);
+	(($log_to_file == "1") ? file_put_contents($log, $output, FILE_APPEND | LOCK_EX) : '');
+	exec("/usr/local/bin/gpio write $other_relay 1", $output);
+	(($log_to_file == "1") ? file_put_contents($log, $output, FILE_APPEND | LOCK_EX) : '');
+	exec("/usr/local/bin/gpio write $light_relay 1", $output);
+	(($log_to_file == "1") ? file_put_contents($log, $output, FILE_APPEND | LOCK_EX) : '');
+	exec("/usr/local/bin/gpio write $door_relay 1", $output);
+	(($log_to_file == "1") ? file_put_contents($log, $output, FILE_APPEND | LOCK_EX) : '');
+	exec("/usr/local/bin/gpio write $lock_relay 1", $output);
+	(($log_to_file == "1") ? file_put_contents($log, $output, FILE_APPEND | LOCK_EX) : '');
+	exec("/usr/local/bin/gpio mode $other_relay OUT", $output);
+	(($log_to_file == "1") ? file_put_contents($log, $output, FILE_APPEND | LOCK_EX) : '');
+	exec("/usr/local/bin/gpio mode $light_relay OUT", $output);
+	(($log_to_file == "1") ? file_put_contents($log, $output, FILE_APPEND | LOCK_EX) : '');
+	exec("/usr/local/bin/gpio mode $door_relay OUT", $output);
+	(($log_to_file == "1") ? file_put_contents($log, $output, FILE_APPEND | LOCK_EX) : '');
+	exec("/usr/local/bin/gpio mode $lock_relay OUT", $output);
+	(($log_to_file == "1") ? file_put_contents($log, $output, FILE_APPEND | LOCK_EX) : '');
+	exec("/usr/local/bin/gpio readall", $output);
+	(($log_to_file == "1") ? file_put_contents($log, $output, FILE_APPEND | LOCK_EX) : '');
+}	
+
+while(true){
+	$command="/sbin/ifconfig $configured_interface | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'";
+	$localIP = exec ($command,$output);
+	(($log_to_file == "1") ? file_put_contents($log, $output, FILE_APPEND | LOCK_EX) : '');
+	echo $localIP;
+	if($localIP != '')
+		break;
 }
 
-if(!$phpqrcode){
+if(!$gat){
+	exec('git clone https://github.com/jamenlang/GarageaTrois-PHP-Server.git GarageaTrois',$output);
+	(($log_to_file == "1") ? file_put_contents($log, $output, FILE_APPEND | LOCK_EX) : '');
+}
+
+if(!$phpqrcode && $qr_enabled == "1"){
 	exec('git clone git://git.code.sf.net/p/phpqrcode/git phpqrcode',$output);
-	file_put_contents($logfile, $output);
+	(($log_to_file == "1") ? file_put_contents($log, $output, FILE_APPEND | LOCK_EX) : '');
 }
 
 if(!$armzilla){
@@ -64,13 +89,14 @@ if(!$armzilla){
 			break;
 		}
         }
-	exec("wget $hue_emulator_link");
+	exec("wget $hue_emulator_link", $output);
+	(($log_to_file == "1") ? file_put_contents($log, $output, FILE_APPEND | LOCK_EX) : '');
 }
 
 if($armzilla == ''){
 	$files = scandir($dir);
 	foreach($files as $filename){
-		file_put_contents($logfile, $filename);
+		file_put_contents($log, $filename, FILE_APPEND | LOCK_EX);
 		if(preg_match('/amazon-echo-bridge/i',$filename,$matches )){
 			$armzilla = $filename;
 		}
@@ -79,10 +105,12 @@ if($armzilla == ''){
 
 if ($armzilla != ''){
 	exec("java -jar $dir$armzilla --upnp.config.address=$localIP");
+	(($log_to_file == "1") ? file_put_contents($log, 'started armzilla hue emulator', FILE_APPEND | LOCK_EX) : '');
+	
 }
 else {
 	$output = 'could not start ' . $dir$armzilla . ' ' . $localIP;
-	file_put_contents($logfile, $output);
+	(($log_to_file == "1") ? file_put_contents($log, $output, FILE_APPEND | LOCK_EX) : '');
 }
 
 ?>
